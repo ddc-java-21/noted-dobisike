1dce8f68834c0ea15b33fda0ad30b3f9b119324e
Dorian <dobisike@cnm.edu>
2025-07-08 13:56:59 -0600

    Added fields, getters and setters to new entity classes Reminder and Task

--- app/src/main/java/edu/cnm/deepdive/noted/model/entity/Reminder.java
+++ app/src/main/java/edu/cnm/deepdive/noted/model/entity/Reminder.java
@@ -1,5 +1,110 @@
 package edu.cnm.deepdive.noted.model.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Entity(
+    tableName = "reminder",
+    foreignKeys = {
+        @ForeignKey(
+            entity = User.class,
+            parentColumns = "user_id",
+            childColumns = "user_id",
+            onDelete = ForeignKey.CASCADE
+        )
+    }
+)
 public class Reminder {
 
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "reminder_id")
+  private long id;
+
+  @NonNull
+  @ColumnInfo(collate = ColumnInfo.NOCASE, index = true)
+  private String title = "";
+
+  private String description;
+
+  @NonNull
+  @ColumnInfo(index = true)
+  private Instant created = Instant.now();
+
+  @NonNull
+  @ColumnInfo(index = true)
+  private Instant modified = Instant.now();
+
+
+  @ColumnInfo(name = "selected_date", index = true)
+  private LocalDateTime selectedDate;
+
+  @ColumnInfo(name = "completed_date", index = true)
+  private boolean completed;
+
+  @ColumnInfo(name = "user_id", index = true)
+  private long userId;
+
+  public long getId() {return id;}
+
+  public Reminder setId(long id) {
+    this.id = id;
+    return this;
+  }
+
+  @NonNull
+  public String getTitle() {return title;}
+
+  public Reminder setTitle(@NonNull String title) {
+    this.title = title;
+    return this;
+  }
+
+  public String getDescription() {return description;}
+
+  public Reminder setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  @NonNull
+  public Instant getCreated() {return created;}
+
+  public Reminder setCreated(@NonNull Instant created) {
+    this.created = created;
+    return this;
+  }
+
+  @NonNull
+  public Instant getModified() {return modified;}
+
+  public Reminder setModified(@NonNull Instant modified) {
+    this.modified = modified;
+    return this;
+  }
+
+  public LocalDateTime getSelectedDate() {return selectedDate;}
+
+  public Reminder setSelectedDate(LocalDateTime selectedDate) {
+    this.selectedDate = selectedDate;
+    return this;
+  }
+
+  public boolean isCompleted() {return completed;}
+
+  public Reminder setCompleted(boolean completed) {
+    this.completed = completed;
+    return this;
+  }
+
+  public long getUserId() {return userId;}
+
+  public Reminder setUserId(long userId) {
+    this.userId = userId;
+    return this;
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/noted/model/entity/Task.java
+++ app/src/main/java/edu/cnm/deepdive/noted/model/entity/Task.java
@@ -1,5 +1,110 @@
 package edu.cnm.deepdive.noted.model.entity;
 
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Entity(
+    tableName = "task",
+    foreignKeys = {
+        @ForeignKey(
+            entity = User.class,
+            parentColumns = "user_id",
+            childColumns = "user_id",
+            onDelete = ForeignKey.CASCADE
+        )
+    }
+)
 public class Task {
 
+  @PrimaryKey(autoGenerate = true)
+  @ColumnInfo(name = "task_id")
+  private long id;
+
+  @NonNull
+  @ColumnInfo(collate = ColumnInfo.NOCASE, index = true)
+  private String title = "";
+
+  private String description;
+
+  @NonNull
+  @ColumnInfo(index = true)
+  private Instant created = Instant.now();
+
+  @NonNull
+  @ColumnInfo(index = true)
+  private Instant modified = Instant.now();
+
+
+  @ColumnInfo(name = "due_date", index = true)
+  private LocalDateTime dueDate;
+
+  @ColumnInfo(name = "completed_date", index = true)
+  private boolean completed;
+
+  @ColumnInfo(name = "user_id", index = true)
+  private long userId;
+
+  public long getId() {return id;}
+
+  public Task setId(long id) {
+    this.id = id;
+    return this;
+  }
+
+  @NonNull
+  public String getTitle() {return title;}
+
+  public Task setTitle(@NonNull String title) {
+    this.title = title;
+    return this;
+  }
+
+  public String getDescription() {return description;}
+
+  public Task setDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  @NonNull
+  public Instant getCreated() {return created;}
+
+  public Task setCreated(@NonNull Instant created) {
+    this.created = created;
+    return this;
+  }
+
+  @NonNull
+  public Instant getModified() {return modified;}
+
+  public Task setModified(@NonNull Instant modified) {
+    this.modified = modified;
+    return this;
+  }
+
+  public LocalDateTime getDueDate() {return dueDate;}
+
+  public Task setDueDate(LocalDateTime dueDate) {
+    this.dueDate = dueDate;
+    return this;
+  }
+
+  public boolean isCompleted() {return completed;}
+
+  public Task setCompleted(boolean completed) {
+    this.completed = completed;
+    return this;
+  }
+
+  public long getUserId() {return userId;}
+
+  public Task setUserId(long userId) {
+    this.userId = userId;
+    return this;
+  }
 }
