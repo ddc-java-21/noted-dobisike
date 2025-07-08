9a9ffd38cdcd86b37305551aac2eacd6a6399ecf
Dorian <dobisike@cnm.edu>
2025-07-08 14:59:58 -0600

    Finished pojo classes for integration with QUERY in TaskDao and ReminderDao classes

--- app/src/main/java/edu/cnm/deepdive/noted/model/pojo/UserWithReminders.java
+++ app/src/main/java/edu/cnm/deepdive/noted/model/pojo/UserWithReminders.java
@@ -1,7 +1,18 @@
 package edu.cnm.deepdive.noted.model.pojo;
 
+import androidx.room.Relation;
+import edu.cnm.deepdive.noted.model.entity.Reminder;
 import edu.cnm.deepdive.noted.model.entity.User;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UserWithReminders extends User {
+
+  @Relation(entity = Reminder.class, parentColumn = "user_id", entityColumn = "user_id")
+  private List<Reminder> reminders = new LinkedList<>();
+
+  public List<Reminder> getReminders() {return reminders;}
+
+  public void setReminders(List<Reminder> reminders) {this.reminders = reminders;}
 
 }
--- app/src/main/java/edu/cnm/deepdive/noted/model/pojo/UserWithTasks.java
+++ app/src/main/java/edu/cnm/deepdive/noted/model/pojo/UserWithTasks.java
@@ -1,7 +1,18 @@
 package edu.cnm.deepdive.noted.model.pojo;
 
+import androidx.room.Relation;
+import edu.cnm.deepdive.noted.model.entity.Task;
 import edu.cnm.deepdive.noted.model.entity.User;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UserWithTasks extends User {
+
+  @Relation(entity = Task.class, parentColumn = "user_id", entityColumn = "user_id")
+  private List<Task> tasks = new LinkedList<>();
+
+  public List<Task> getTasks() {return tasks;}
+
+  public void setTasks(List<Task> tasks) {this.tasks = tasks;}
 
 }
