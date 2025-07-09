6840edee4c1e4e26539f29f1d3bfccaae8a91d68
Dorian <dobisike@cnm.edu>
2025-07-09 16:40:11 -0600

    Got through a little bit of Task Repository (not working).
    (cherry picked from commit 4ed9ed1ed34b5c468dcdb9db9ce091ef1d3d433b)

--- app/src/main/java/edu/cnm/deepdive/noted/service/repository/TaskRepository.java
+++ app/src/main/java/edu/cnm/deepdive/noted/service/repository/TaskRepository.java
@@ -1,10 +1,15 @@
 package edu.cnm.deepdive.noted.service.repository;
 
 import androidx.lifecycle.LiveData;
+import edu.cnm.deepdive.noted.model.entity.Task;
+import edu.cnm.deepdive.noted.model.entity.User;
 import edu.cnm.deepdive.noted.model.pojo.UserWithTasks;
 import edu.cnm.deepdive.noted.service.dao.TaskDao;
+import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.schedulers.Schedulers;
+import java.util.List;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -23,6 +28,27 @@ public class TaskRepository {
 
   public LiveData<UserWithTasks> get(long taskId) {
     return taskDao.select(taskId);
+  }
+
+  public Completable remove(Task task) {
+    return taskDao
+        .delete(task)
+        .subscribeOn(scheduler)
+        .ignoreElement();
+  }
+
+  public Single<UserWithTasks> save(UserWithTasks user, Task task) {
+    task.setUserId(user.getId());
+    return (
+        (task.getId() == 0) ? taskDao.insert(task) : taskDao.update(task)
+        )
+        .doOnSuccess((u) -> user.getTasks().forEach((t) -> task.setUserId(u.getId())))
+        .map((u) -> user.getTasks())
+        .
+  }
+
+  private Single<List<Task>> saveTasks(List<Task> tasks) {
+
   }
 
 }
