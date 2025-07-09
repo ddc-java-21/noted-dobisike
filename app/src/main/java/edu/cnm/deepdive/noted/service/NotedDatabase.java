c8cfd3b41b07d1ccbb34d6996a58c711317f0942
Dorian <dobisike@cnm.edu>
2025-07-09 11:17:24 -0600

    Added getTaskDao and getReminderDao to NotedDatabase

--- app/src/main/java/edu/cnm/deepdive/noted/service/NotedDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/noted/service/NotedDatabase.java
@@ -7,15 +7,21 @@ import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 import edu.cnm.deepdive.noted.model.entity.Image;
 import edu.cnm.deepdive.noted.model.entity.Note;
+import edu.cnm.deepdive.noted.model.entity.Reminder;
+import edu.cnm.deepdive.noted.model.entity.Task;
 import edu.cnm.deepdive.noted.model.entity.User;
 import edu.cnm.deepdive.noted.service.NotedDatabase.Converters;
 import edu.cnm.deepdive.noted.service.dao.ImageDao;
 import edu.cnm.deepdive.noted.service.dao.NoteDao;
+import edu.cnm.deepdive.noted.service.dao.ReminderDao;
+import edu.cnm.deepdive.noted.service.dao.TaskDao;
 import edu.cnm.deepdive.noted.service.dao.UserDao;
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Database(
-    entities = {User.class, Note.class, Image.class},
+    entities = {User.class, Task.class, Reminder.class, Note.class, Image.class},
     version = NotedDatabase.VERSION
 )
 @TypeConverters(Converters.class)
@@ -29,6 +35,10 @@ public abstract class NotedDatabase extends RoomDatabase {
   }
 
   public abstract UserDao getUserDao();
+
+  public abstract TaskDao getTaskDao();
+
+  public abstract ReminderDao getReminderDao();
 
   public abstract NoteDao getNoteDao();
 
