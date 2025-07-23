a69e308a106a7f1af70f5c87f5f41066c3194232
Dorian <dobisike@cnm.edu>
2025-07-23 15:10:22 -0600

    Created basic layout of DayBinder, created HeaderBinder, string resources for dates and formats, made additions to calendarFragment in onCreateView

---
+++ app/src/main/java/edu/cnm/deepdive/noted/controller/adapter/DayBinder.java
@@ -0,0 +1,131 @@
+package edu.cnm.deepdive.noted.controller.adapter;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import com.kizitonwose.calendar.core.CalendarDay;
+import com.kizitonwose.calendar.core.DayPosition;
+import com.kizitonwose.calendar.view.MonthDayBinder;
+import com.kizitonwose.calendar.view.ViewContainer;
+import dagger.hilt.android.qualifiers.ActivityContext;
+import dagger.hilt.android.scopes.FragmentScoped;
+import edu.cnm.deepdive.noted.R;
+import edu.cnm.deepdive.noted.databinding.DayCalendarBinding;
+import edu.cnm.deepdive.noted.model.entity.Reminder;
+import edu.cnm.deepdive.noted.model.entity.Task;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import javax.inject.Inject;
+
+@FragmentScoped
+public class DayBinder implements MonthDayBinder<ViewContainer> {
+
+  private static final String TAG = DayBinder.class.getSimpleName();
+
+  private final Map<LocalDate, Reminder> reminderMap;
+  private final Map<LocalDate, Task> taskMap;
+
+  private OnReminderClickListener reminderClickListener;
+  private OnTaskClickListener taskClickListener;
+
+  @Inject
+  public DayBinder() {
+    this.reminderMap = new  HashMap<>();
+    this.taskMap = new  HashMap<>();
+    reminderClickListener = (reminder) -> {};
+    taskClickListener = (task) -> {};
+  }
+
+  @Inject
+
+
+  @NonNull
+  @Override
+  public ViewContainer create(@NonNull View view) {
+    return new DayHolder(view);
+  }
+
+  @Override
+  public void bind(@NonNull ViewContainer holder, CalendarDay calendarDay) {
+    ((DayHolder) holder).bind(calendarDay);
+  }
+
+  public class DayHolder extends ViewContainer {
+
+    private final DayCalendarBinding binding;
+    private final Drawable clickableBackground;
+
+    private Reminder reminder;
+    private Task task;
+
+    public DayHolder(@NonNull View view) {
+      super(view);
+      binding = DayCalendarBinding.bind(view);
+      clickableBackground = view.getBackground();
+    }
+
+    public void bind(CalendarDay calendarDay) {
+      TextView dayText = binding.getRoot();
+      dayText.setText(String.valueOf(calendarDay.getDate().getDayOfMonth()));
+      Reminder reminder = reminderMap.get(calendarDay.getDate());
+      Task task = taskMap.get(calendarDay.getDate());
+      if (reminder != null) {
+        this.reminder = reminder;
+        dayText.setClickable(true);
+        dayText.setFocusable(true);
+        dayText.setOnClickListener(this::translateClicks);
+        dayText.setBackground(clickableBackground);
+        dayText.setTextAppearance(
+            (calendarDay.getPosition() == DayPosition.MonthDate)
+                ? R.style.CalendarTextAppearance_AvailableDay
+                : R.style.CalendarTextAppearance_AvailableDay_OutOfMonth
+        );
+      } else {
+        dayText.setClickable(false);
+        dayText.setFocusable(false);
+        dayText.setOnClickListener(null);
+        dayText.setBackground(null);
+        dayText.setTextAppearance(R.style.CalendarTextAppearance);
+      }
+      if (task != null) {
+        this.task = task;
+        dayText.setClickable(true);
+        dayText.setFocusable(true);
+        dayText.setOnClickListener(this::translateClicks);
+        dayText.setBackground(clickableBackground);
+        dayText.setTextAppearance(
+            (calendarDay.getPosition() == DayPosition.MonthDate)
+                ? R.style.CalendarTextAppearance_AvailableDay
+                : R.style.CalendarTextAppearance_AvailableDay_OutOfMonth
+        );
+      } else {
+        dayText.setClickable(false);
+        dayText.setFocusable(false);
+        dayText.setOnClickListener(null);
+        dayText.setBackground(null);
+        dayText.setTextAppearance(R.style.CalendarTextAppearance);
+      }
+    }
+
+    private void translateClicks(View view) {
+      reminderClickListener.onReminderClick(reminder);
+      taskClickListener.onTaskClick(task);
+    }
+
+  }
+
+  @FunctionalInterface
+  public interface OnTaskClickListener {
+
+    void onTaskClick(Task task);
+  }
+
+  @FunctionalInterface
+  public interface OnReminderClickListener {
+
+    void onReminderClick(Reminder reminder);
+  }
+}
--- app/src/main/java/edu/cnm/deepdive/noted/controller/calendar/CalendarFragment.java
+++ app/src/main/java/edu/cnm/deepdive/noted/controller/calendar/CalendarFragment.java
@@ -8,24 +8,48 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.noted.R;
+import edu.cnm.deepdive.noted.controller.adapter.DayBinder;
+import edu.cnm.deepdive.noted.databinding.CalendarDayLayoutBinding;
 import edu.cnm.deepdive.noted.databinding.FragmentCalendarBinding;
+import edu.cnm.deepdive.noted.databinding.HeaderCalenderBinding;
+import edu.cnm.deepdive.noted.viewmodel.ReminderViewModel;
+import edu.cnm.deepdive.noted.viewmodel.TaskViewModel;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.MonthDay;
 import java.time.YearMonth;
+import java.time.temporal.WeekFields;
+import java.util.Locale;
+import javax.inject.Inject;
 
 @AndroidEntryPoint
 public class CalendarFragment extends Fragment {
+
+  @Inject
+  DayBinder dayBinder;
+  @Inject
+  HeaderCalenderBinding headerBinder;
 
   private static final String TAG = CalendarFragment.class.getSimpleName();
 
   private FragmentCalendarBinding binding;
   private YearMonth selectedMonth;
-  private MonthDay selectedDay;
+  private ReminderViewModel reminderViewModel;
+  private TaskViewModel taskViewModel;
 
 
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
       @Nullable Bundle savedInstanceState) {
+    LocalDate firstReminderDate = LocalDate.parse(getString(R.string.first_reminder_date));
+    LocalDate firstTaskDate = LocalDate.parse(getString(R.string.first_task_date));
+    YearMonth firstReminderMonth = YearMonth.from(firstReminderDate);
+    YearMonth firstTaskMonth = YearMonth.from(firstTaskDate);
+    DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+    YearMonth currentMonth = YearMonth.now();
+
     return super.onCreateView(inflater, container, savedInstanceState);
   }
 
