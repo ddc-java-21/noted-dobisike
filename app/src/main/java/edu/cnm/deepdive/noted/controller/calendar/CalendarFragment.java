39a622f4a99366f9609abfc5efd072f010ebc4c8
Dorian <dobisike@cnm.edu>
2025-07-23 10:59:09 -0600

    Added neccessary variables and imported oncreateview, onviewcreated, and ondestroyview overrrides.

--- app/src/main/java/edu/cnm/deepdive/noted/controller/calendar/CalendarFragment.java
+++ app/src/main/java/edu/cnm/deepdive/noted/controller/calendar/CalendarFragment.java
@@ -1,13 +1,41 @@
 package edu.cnm.deepdive.noted.controller.calendar;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.noted.databinding.FragmentCalendarBinding;
+import java.time.MonthDay;
+import java.time.YearMonth;
 
 @AndroidEntryPoint
 public class CalendarFragment extends Fragment {
 
   private static final String TAG = CalendarFragment.class.getSimpleName();
 
-  DayBind
+  private FragmentCalendarBinding binding;
+  private YearMonth selectedMonth;
+  private MonthDay selectedDay;
 
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    return super.onCreateView(inflater, container, savedInstanceState);
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+  }
+
+  @Override
+  public void onDestroyView() {
+    super.onDestroyView();
+  }
 }
